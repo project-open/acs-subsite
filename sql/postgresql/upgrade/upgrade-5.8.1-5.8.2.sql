0639b56468a75a7fa74a8ebc5a08bdffd272b21e
Frank Bergmann <frank.bergmann@project-open.com>
2016-01-05 12:46:53 +0000

    - added if exists to avoid errors

--- sql/postgresql/upgrade/upgrade-5.8.1-5.8.2.sql
+++ sql/postgresql/upgrade/upgrade-5.8.1-5.8.2.sql
@@ -1,2 +1,2 @@
-alter table application_groups drop constraint application_groups_group_id_fk;
-alter table application_groups add constraint application_groups_group_id_fk foreign key (group_id) references groups(group_id) on delete cascade;
+alter table application_groups drop constraint if exists application_groups_group_id_fk;
+alter table application_groups add constraint if exists application_groups_group_id_fk foreign key (group_id) references groups(group_id) on delete cascade;
